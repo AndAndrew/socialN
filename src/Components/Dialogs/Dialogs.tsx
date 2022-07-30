bcf227ad0c8860ee7af03d1e56645542455faff9
andAndrew <andAndrew.k@gmail.com>
2022-07-30 13:47:35 +0400

    feat: added Dialogs styles

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -3,8 +3,26 @@ import s from './Dialogs.module.css';
 
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Dimych
+                </div>
+                <div className={s.dialog}>
+                    Andrew
+                </div>
+                <div className={s.dialog}>
+                    Den
+                </div>
+                <div className={s.dialog}>
+                    Max
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Yo!</div>
+                <div className={s.message}>What's hap?</div>
+                <div className={s.message}>How are your job?</div>
+            </div>
         </div>
     )
 }
