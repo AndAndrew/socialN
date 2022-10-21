cdfac1b4a62e66c71037d57e1eac3ec3d11b2224
andAndrew <andAndrew.k@gmail.com>
2022-10-21 13:10:45 +0400

    feat: added ProfileStatus component

--- src/Components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/Components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import s from './ProfileInfo.module.css'
 import {Preloader} from "../../Common/Preloader/Preloader";
+import {ProfileStatus} from "./ProfileStatus";
 
 type PropsType = {
     profile: any
@@ -12,14 +13,14 @@ export const ProfileInfo = (props: PropsType) => {
     }
     return (
         <div>
-            <div>
-                <img className={s.image}
-                     src='https://blog.ostrovok.ru/wp-content/uploads/2020/01/ostrovok-filters-4-123456-2.png'
-                     alt='content image'/>
-            </div>
+            {/*<div>*/}
+            {/*    <img className={s.image}*/}
+            {/*         src='https://blog.ostrovok.ru/wp-content/uploads/2020/01/ostrovok-filters-4-123456-2.png'*/}
+            {/*         alt='content image'/>*/}
+            {/*</div>*/}
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.large}/>
-                ava+descr
+                <ProfileStatus status={'Hello'}/>
             </div>
         </div>
     )
